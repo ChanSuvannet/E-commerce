37534023483534d5444c3fa4e4aa80b035dda277
SreypichChheang <chheangsreypich353@gmail.com>
2024-12-05 22:34:38 +0700

    Create footer

--- src/main.js
+++ src/main.js
@@ -12,6 +12,9 @@ import '@core/scss/template/index.scss'
 import '@styles/styles.scss'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+// main.js or App.vue
+import '@fortawesome/fontawesome-free/css/all.css'
+
 
 loadFonts()
 
