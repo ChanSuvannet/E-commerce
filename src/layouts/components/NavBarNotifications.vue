532168dbad8d78aa2d694b9e3c9da69025862bd4
CHAN Suvannet <chansuvannet999@gmail.com>
2024-12-05 12:27:47 +0700

    fix eror

--- src/layouts/components/NavBarNotifications.vue
+++ src/layouts/components/NavBarNotifications.vue
@@ -1,8 +1,5 @@
 <script setup>
-import avatar3 from '@images/avatars/avatar-3.png'
 import avatar4 from '@images/avatars/avatar-4.png'
-import avatar5 from '@images/avatars/avatar-5.png'
-import paypal from '@images/svg/paypal.svg'
 
 const notifications = ref([
   {
@@ -11,39 +8,6 @@ const notifications = ref([
     title: 'Congratulation Flora! 🎉',
     subtitle: 'Won the monthly best seller badge',
     time: 'Today',
-    isSeen: true,
-  },
-  {
-    id: 2,
-    text: 'Tom Holland',
-    title: 'New user registered.',
-    subtitle: '5 hours ago',
-    time: 'Yesterday',
-    isSeen: false,
-  },
-  {
-    id: 3,
-    img: avatar5,
-    title: 'New message received 👋🏻',
-    subtitle: 'You have 10 unread messages',
-    time: '11 Aug',
-    isSeen: true,
-  },
-  {
-    id: 4,
-    img: paypal,
-    title: 'Paypal',
-    subtitle: 'Received Payment',
-    time: '25 May',
-    isSeen: false,
-    color: 'error',
-  },
-  {
-    id: 5,
-    img: avatar3,
-    title: 'Received Order 📦',
-    subtitle: 'New order received from john',
-    time: '19 Mar',
     isSeen: true,
   },
 ])
