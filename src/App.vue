f3109744d494c965b9d14bfea62f32d441bbdf7c
CHAN Suvannet <chansuvannet999@gmail.com>
2024-12-19 11:41:04 +0700

    push code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
-import Header from './components/Header.vue';
+import { computed } from "@vue/runtime-dom";
+import { useRoute } from "vue-router";
+import Header from "./components/Header.vue";
+
+// Get the current route
+const route = useRoute();
+
+// List of routes where the header should be hidden
+const hiddenHeaderPaths = ["/register", "/login"];
+
+const isHeaderHidden = computed(() => hiddenHeaderPaths.includes(route.path));
 </script>
 
 <template>
-  <Header></Header>
-  <RouterView></RouterView>
+  <div :class="{ hidden: isHeaderHidden }">
+    <Header />
+  </div>
+  <RouterView />
 </template>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+</style>
