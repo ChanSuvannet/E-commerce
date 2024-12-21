ef1a1272d53890c45a555df8216ef2e030b1ed62
CHOR NARIN <narintkh178@gmail.com>
2024-12-21 23:11:46 +0700

    update

--- src/components/Shop/Shop.vue
+++ src/components/Shop/Shop.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-red-500 text-[100px]">what the fuck </div>
+  <div class="bg-red-500 text-[100px]">what the  </div>
+  <h1>This is what I want to do </h1>
 </template>
 
