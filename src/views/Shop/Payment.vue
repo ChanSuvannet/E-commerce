f0cddbec0cfb3245fe1330e12182894e4662b4ff
CHAN Suvannet <chansuvannet999@gmail.com>
2025-01-14 19:10:23 +0700

    fix error on payment

--- src/views/Shop/Payment.vue
+++ src/views/Shop/Payment.vue
@@ -38,7 +38,6 @@
                   id="card-number-input"
                   class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pe-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500"
                   placeholder="xxxx-xxxx-xxxx-xxxx"
-                  pattern="^4[0-9]{12}(?:[0-9]{3})?$"
                   required
                 />
               </div>
