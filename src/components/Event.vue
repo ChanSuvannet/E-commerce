d1c1de26430ea9fea10e6930e4fed6f1da4bf94f
CHAN Suvannet <chansuvannet999@gmail.com>
2025-01-14 20:45:10 +0700

    add comment on event

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -113,6 +113,181 @@
         </router-link>
       </div>
     </div>
+
+    <section class="py-24 relative">
+      <div class="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+        <div
+          class="w-full flex-col justify-start items-start lg:gap-14 gap-7 inline-flex"
+        >
+          <h2
+            class="text-[#022d5a] text-xl font-bold font-manrope leading-normal"
+          >
+            Comments & Feedback
+          </h2>
+          <div class="w-full flex-col justify-start items-start gap-8 flex">
+            <!-- Comment 1 -->
+            <div
+              class="w-full lg:p-8 p-5 bg-white rounded-3xl border border-gray-200 flex-col justify-start items-start flex"
+            >
+              <div
+                class="w-full flex-col justify-start items-start gap-3.5 flex"
+              >
+                <div class="w-full justify-between items-center inline-flex">
+                  <div class="justify-start items-center gap-2.5 flex">
+                    <div
+                      class="w-10 h-10 bg-stone-300 rounded-full justify-start items-start gap-2.5 flex"
+                    >
+                      <img
+                        class="rounded-full object-cover"
+                        src="https://pagedone.io/asset/uploads/1710225753.png"
+                        alt="John Smith image"
+                      />
+                    </div>
+                    <div
+                      class="flex-col justify-start items-start gap-1 inline-flex"
+                    >
+                      <h5
+                        class="text-gray-900 text-sm font-semibold leading-snug"
+                      >
+                        John Smith
+                      </h5>
+                      <h6 class="text-gray-500 text-xs font-normal leading-5">
+                        5 Days ago
+                      </h6>
+                    </div>
+                  </div>
+                  <div class="group justify-end items-center flex">
+                    <div
+                      class="px-5 py-2.5 rounded-xl shadow-lg border border-gray-400 hover:border-[#022d5a] transition-all duration-700 ease-in-out justify-center items-center flex"
+                    >
+                      <a href="#" class="">
+                        <svg
+                          class="group-hover:text-[#022d5a] text-gray-400 group-hover:fill-[#022d5a] fill-white transition-all duration-700 ease-in-out"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M2.62629 3.43257C4.64001 1.44869 7.82082 1.31134 9.99614 3.02053C12.1723 1.31134 15.3589 1.44869 17.3726 3.43257L17.3734 3.43334C19.5412 5.57611 19.5412 9.04382 17.3804 11.1867L17.378 11.1891L10.4631 17.9764C10.2035 18.2312 9.78765 18.2309 9.52844 17.9758L2.62629 11.1821C0.457252 9.04516 0.457252 5.56947 2.62629 3.43257Z"
+                            stroke="currentColor"
+                          />
+                        </svg>
+                      </a>
+                      <div class="px-2 justify-center items-center flex">
+                        <h5
+                          class="group-hover:text-[#022d5a] text-gray-400 transition-all duration-700 ease-in-out text-base font-semibold leading-relaxed"
+                        >
+                          34
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p class="text-gray-800 text-sm font-normal leading-snug">
+                  This product is great! The discount was applied, and it works
+                  well for my needs.
+                </p>
+              </div>
+            </div>
+
+            <!-- Comment 2 -->
+            <div
+              class="w-full lg:p-8 p-5 bg-white rounded-3xl border border-gray-200 flex-col justify-start items-start gap-2.5 flex"
+            >
+              <div
+                class="w-full flex-col justify-start items-start gap-3.5 flex"
+              >
+                <div class="w-full justify-between items-center inline-flex">
+                  <div class="justify-start items-center gap-2.5 flex">
+                    <div
+                      class="w-10 h-10 bg-stone-300 rounded-full justify-start items-start gap-2.5 flex"
+                    >
+                      <img
+                        class="w-10 h-10 rounded-full object-cover"
+                        src="https://pagedone.io/asset/uploads/1710238051.png"
+                        alt="Emma Davis image"
+                      />
+                    </div>
+                    <div
+                      class="flex-col justify-start items-start gap-1 inline-flex"
+                    >
+                      <h5
+                        class="text-gray-900 text-sm font-semibold leading-snug"
+                      >
+                        Emma Davis
+                      </h5>
+                      <h6 class="text-gray-500 text-xs font-normal leading-5">
+                        4 Days ago
+                      </h6>
+                    </div>
+                  </div>
+                  <div class="group justify-end items-center flex">
+                    <div
+                      class="px-5 py-2.5 rounded-xl shadow-lg border border-gray-400 hover:border-[#022d5a] transition-all duration-700 ease-in-out justify-center items-center flex"
+                    >
+                      <a href="#" class="">
+                        <svg
+                          class="group-hover:text-[#022d5a] text-gray-400 group-hover:fill-[#022d5a] fill-white transition-all duration-700 ease-in-out"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path
+                            d="M2.62629 3.43257C4.64001 1.44869 7.82082 1.31134 9.99614 3.02053C12.1723 1.31134 15.3589 1.44869 17.3726 3.43257L17.3734 3.43334C19.5412 5.57611 19.5412 9.04382 17.3804 11.1867L17.378 11.1891L10.4631 17.9764C10.2035 18.2312 9.78765 18.2309 9.52844 17.9758L2.62629 11.1821C0.457252 9.04516 0.457252 5.56947 2.62629 3.43257Z"
+                            stroke="currentColor"
+                          />
+                        </svg>
+                      </a>
+                      <div class="px-2 justify-center items-center flex">
+                        <h6
+                          class="group-hover:text-[#022d5a] text-gray-400 transition-all duration-700 ease-in-out text-base font-semibold leading-relaxed"
+                        >
+                          30
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p class="text-gray-800 text-sm font-normal leading-snug">
+                  The item is as expected and works great with the discount
+                  applied. Happy with the purchase!
+                </p>
+              </div>
+            </div>
+
+            <!-- Comment Input -->
+            <div class="w-full relative flex justify-between gap-2">
+              <input
+                type="text"
+                class="w-full py-3 px-5 rounded-lg border border-gray-300 bg-white shadow-md focus:outline-none text-gray-900 placeholder-gray-400 text-lg font-normal leading-relaxed"
+                placeholder="Leave a constructive comment..."
+              />
+              <a href="#" class="absolute right-6 top-[18px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <path
+                    d="M10.0194 1.66699V5.6556C1.69526 5.6556 1.54178 14.4163 1.69573 18.3337C1.69573 16.4818 5.84659 10.0003 10.0194 10.6414V14.63L18.3332 8.14847L10.0194 1.66699Z"
+                    stroke="#111827"
+                    stroke-width="1.6"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <br />
   </div>
 </template>
